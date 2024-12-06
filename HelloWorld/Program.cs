3eb23e21f1824564535b95a277218b03a576f758
Samanta Melissa <samanta.melissa13@gmail.com>
2024-12-06 16:54:45 -0300

    Subindo primeira aula

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine -> Escreve no nosso console
+//Console.ReadLine -> Lê o dado do console, nesse caso: Escrito pelo usuário.
+//Criar variável: tipododado nomedavariavel
+Console.WriteLine("Olá, Lucas!");
+Console.WriteLine("Você está bem?");
+
+Console.WriteLine("Qual a sua comida favorita?");
+string comida = Console.ReadLine();
+
+Console.WriteLine($"Que legal, Lucas! Sua comida favorita é: {comida}");
+
+Console.WriteLine("Lucas, escolha alguém para falar a comida favorita dela");
+string nomePessoaNova = Console.ReadLine()!;
+
+Console.WriteLine($"Seja bem vindo {nomePessoaNova}, qual sua comida favorita?");
+string comidaNova = Console.ReadLine()!;
+
+Console.WriteLine($"Que legal, {nomePessoaNova} ! Sua comida favorita é: {comidaNova}");
+
+//int
+//bool
+//float
+
